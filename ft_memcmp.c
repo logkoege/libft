250a608505327cab8532bfbbb738dc76c1d45cab
logkoege <togokoegele@gmail.com>
2024-04-24 17:08:44 +0200

    l

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 17:05:02 by logkoege          #+#    #+#             */
+/*   Updated: 2024/04/24 17:07:45 by logkoege         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*a;
+	unsigned char	*b;
+
+	a = (unsigned char *)s1;
+	b = (unsigned char *)s2;
+	while (n > 0)
+	{
+		if (*a != *b)
+			return (*a - *b);
+		a++;
+		b++;
+		n--;
+	}
+	return (0);
+}
