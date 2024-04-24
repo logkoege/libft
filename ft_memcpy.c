3cbf383f89f9475fab39d57773a829dba8cb4161
logkoege <togokoegele@gmail.com>
2024-04-24 17:03:56 +0200

    l

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,25 @@
 /*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 12:32:06 by logkoege          #+#    #+#             */
-/*   Updated: 2024/04/24 12:33:02 by logkoege         ###   ########.fr       */
+/*   Updated: 2024/04/24 17:01:15 by logkoege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		*(unsigned char*)(dest + i) = *(unsigned char*)(src);
+		i++;
+		src++;
+	}
+	return (dest);
+}
 
